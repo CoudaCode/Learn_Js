193377702aa269f4a8e3a14c7b18d6cae8e81914
Diarassouba Mamadou <couda.dm@gmail.com>
2023-03-23 14:12:29 +0000

    demo

--- Snake/script.js
+++ Snake/script.js
@@ -204,19 +204,19 @@
 // console.log(data(12,9).divise())
 
 
-let Obj = {
-  x:10,
-  y:20,
-  func:function(){
-    console.log(this)
-    return this.x
-  }
-}
+// let Obj = {
+//   x:10,
+//   y:20,
+//   func:function(){
+//     console.log(this)
+//     return this.x
+//   }
+// }
 
-// this.x = 15
-let dira = Obj.func
+// // this.x = 15
+// let dira = Obj.func
 
-console.log(dira())
+// console.log(dira())
 
 // console.log(Obj.func())
 
@@ -232,6 +232,79 @@ console.log(dira())
 // console.log('test',diara())
 // console.log(nome())
 
+let Obj ={
+    x :10,
+    y:20,
+    myFunc(){
+        console.log(this)
+    }
+}
+let PObj ={
+  log:10,
+  lp:20,
+  
+}
 
 
 
+let diara = Obj.myFunc.bind(Obj)
+console.log(diara())
+/******************************** Expressions regulières **************************************/
+//Expression Regulieres
+//Recherche des caractères dans une chaine de caractères donnés
+
+// g : global; i:insanssible à la casse
+
+
+// recherche un caratere on utilise la methode .test();  Retourne True(s'il existe) ou False(existe pas)
+//
+// exple :    
+/* Code */
+// let exampleStr = "Let's have fun with regular expressions!";
+// let unRegex = /fun/gi; // Change this line
+// let result = unRegex.test(exampleStr);
+
+
+// il retoune vrai
+ /* fin code */
+
+
+
+//  Retourne Plusieurs Elements recherché dans un tableau avec Match
+
+//    let exampleStr = "Let's have fun with Have regular expressions!";
+//    let unRegex = /have/gi; // Change this line
+//    let result = exampleStr.match(unRegex);
+
+
+
+//  retourne --->  ['have','Have']
+
+
+// Faites correspondre n'importe quoi avec la période générique(on utlise le point).
+
+// let exampleStr = "Let's have fun with regular expressions!";
+// let unRegex = /un./; // Change this line
+// let result = unRegex.test(exampleStr);
+
+// retourne --->  true
+
+
+
+//Utilisez une classe de caractères avec des voyelles ( a, e, i, o, u) dans votre regex vowelRegexpour trouver toutes les voyelles de la chaîne quoteSample.
+
+// let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+// let vowelRegex = /[aeiou]/gi; // Change this line
+// let result = quoteSample.match(vowelRegex); // Change this line
+
+//return --> ['e', 'a', 'e', 'o', 'u', 'i', 'e', 'a', 'o', 'e', 'o', 'e', 'I', 'a', 'e', 'o', 'o', 'e', 'i', 'o', 'e', 'o', 'i', 'e', 'i']
+
+
+// Associez les lettres de l'alphabet
+
+// let quoteSample = "The quick brown fox jumps over the lazy dog.";
+// let alphabetRegex = /[a-z]/gi; // Change this line
+// let result = quoteSample.match(alphabetRegex); // Change this line
+
+/******************************** Expressions regulières **************************************/
+// 
